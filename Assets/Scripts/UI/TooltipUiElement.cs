6fb61d98da9900312f4f29723815dfd60aef47f7
ttetrafon <ttetrafon@yahoo.gr>
2021-03-30 19:38:22 +0100

    fixed tooltip position when it is overflowing the screen

--- Assets/Scripts/UI/TooltipUiElement.cs
+++ Assets/Scripts/UI/TooltipUiElement.cs
@@ -16,11 +16,12 @@ public class TooltipUiElement : MonoBehaviour {
     Vector2 pos = InputController.controls.Common.MousePosition.ReadValue<Vector2>() + standardOffset;
     // Debug.Log("mouse position: " + pos.ToString());
     // Debug.Log("screen size: " + screenSize.ToString());
+    // Debug.Log("text size: " + textSize.ToString());
     if (pos.x + textSize.x > screenSize.x) {
-      pos.x -= textSize.x - standardOffset.x;
+      pos.x -= (textSize.x + standardOffset.x);
     }
     if (pos.y + textSize.y > screenSize.y) {
-      pos.y -= textSize.y - standardOffset.y;
+      pos.y -= 2 * (textSize.y + standardOffset.y);
     }
     // Debug.Log("tooltip size: " + gameObject.GetComponent<RectTransform>().sizeDelta);
     this.gameObject.transform.position = pos;
